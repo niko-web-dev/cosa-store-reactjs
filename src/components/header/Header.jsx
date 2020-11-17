5318258f3152c821a23e19c9aeb229da198bef75
Niko o <kolyabokov41@gmail.com>
2020-11-17 18:57:34 +0300

    frist setting

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import s from './header.module.scss'
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+    
+    </header>
+  )
+}
+
+export default Header
